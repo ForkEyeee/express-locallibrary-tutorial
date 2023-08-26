44eb72dd3838fabe5fe20dbdd9ca71a335e9cc21
ForkEyeee <shawncarter123456@gmail.com>
2023-08-26 17:14:20 -0400

    debugging app.js

--- app.js
+++ app.js
@@ -3,22 +3,31 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const dev_db_url = require("./dev_db_url");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-const catalogRouter = require("./routes/catalog.js");
+const catalogRouter = require("./routes/catalog.js"); //Import routes for "catalog" area of site
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
+// Compress all routes
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 2000,
 });
-
 // Apply rate limiter to all requests
 app.use(limiter);
 app.use(logger("dev"));
@@ -26,10 +35,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(compression());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -39,21 +49,21 @@ app.use(function (req, res, next) {
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+// Set up mongoose connection
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-const mongoDB = process.env.MONGODB_URI;
-mongoose
-  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started on port", process.env.PORT || 3000);
-});
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // error handler
 app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
