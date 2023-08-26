0f661e4de636d3d177465dd13d261e7032de011e
ForkEyeee <shawncarter123456@gmail.com>
2023-08-26 16:36:52 -0400

    fix cannot find moduel compression

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.use(compression()); // Compress all routes
+// Compress all routes
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(compression());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
